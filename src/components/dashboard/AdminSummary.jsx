a413c09861688aece3a666bf9a6cf90f96f529fa
immahesh555 <themahiece@gmail.com>
2024-12-05 10:53:44 +0530

    summarycolor

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -34,8 +34,8 @@ const AdminSummary = () => {
     <div className='p-6'>
       <h3 className='text-2xl font-bold'>Dashboard Overview</h3>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-6'>
-        <SummaryCard icon={<FaUsers />} text="Total Checksheets" number={summary.totalEmployees} color="bg-teal-600" />
-        <SummaryCard icon={<FaBuilding />} text="Total Departments" number={summary.totalDepartments} color="bg-yellow-600" />
+        <SummaryCard icon={<FaUsers />} text="Total Checksheets" number={summary.totalEmployees} color="bg-bright-teal" />
+        <SummaryCard icon={<FaBuilding />} text="Total Departments" number={summary.totalDepartments} color="bg-bright-yellow" />
 {       /* <SummaryCard icon={<FaMoneyBillWave />} text="Monthly Salary" number="$654" color="bg-red-600" />
 */}      </div>
 
@@ -43,10 +43,10 @@ const AdminSummary = () => {
         <h4 className='text-center text-2xl font-bold'>CheckSheet Details</h4>
 
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
-          <SummaryCard icon={<FaFileAlt />} text="CheckSheet Applied" number={summary.leaveSummary.appliedFor} color="bg-teal-600" />
-          <SummaryCard icon={<FaCheckCircle />} text="CheckSheet Approved" number={summary.leaveSummary.approved} color="bg-green-600" />
-          <SummaryCard icon={<FaHourglassHalf />} text="CheckSheet Pending" number={summary.leaveSummary.pending} color="bg-yellow-600" />
-          <SummaryCard icon={<FaTimesCircle />} text="CheckSheet Rejected" number={summary.leaveSummary.rejected} color="bg-red-600" />
+          <SummaryCard icon={<FaFileAlt />} text="CheckSheet Applied" number={summary.leaveSummary.appliedFor} color="bg-electric-blue" />
+          <SummaryCard icon={<FaCheckCircle />} text="CheckSheet Approved" number={summary.leaveSummary.approved} color="bg-neon-green" />
+          <SummaryCard icon={<FaHourglassHalf />} text="CheckSheet Pending" number={summary.leaveSummary.pending} color="bg-bright-yellow" />
+          <SummaryCard icon={<FaTimesCircle />} text="CheckSheet Rejected" number={summary.leaveSummary.rejected} color="bg-bright-red" />
 
 
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,13 @@ export default {
         "Doto":['Doto','sans-serif']
       },
       colors: {
-        'custom-purple': '#2a2b84', // Add your custom color here
+        'custom-purple': '#2a2b84',
+        'bright-teal':'#00FFD1',
+        'bright-yellow':'#FFFF00',
+        'neon-green':'#39FF14',
+        'electric-blue':'#007FFF',
+        'luminous-cyan':'#00FFFF',
+        'bright-red':'#FF0000'
       },
     },
   },
