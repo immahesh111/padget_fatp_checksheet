127cd00e42d8ae5a03f9f37a040d6951132d1152
immahesh555 <themahiece@gmail.com>
2024-12-05 12:22:33 +0530

    afejuhefj

--- src/components/EmployeeDashboard/Sidebar.jsx
+++ src/components/EmployeeDashboard/Sidebar.jsx
@@ -8,42 +8,70 @@ const Sidebar = ({ isOpen }) => {
   const { user } = useAuth();
 
   return (
-    <div className={`bg-gray-800 text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64 transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
-      <div className='bg-teal-600 h-12 flex items-center justify-center'>
+    <div className={`bg-custom-purple text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64 transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
+      <div className='bg-white h-12 flex items-center justify-center'>
         <img src={logo} alt="CheckSheets Logo" className='h-10' />
       </div>
       <div className='px-4'>
-        <NavLink 
+        <NavLink
           to="/employee-dashboard"
-          className={({ isActive }) => `${isActive ? "bg-teal-500" : ""} flex items-center space-x-4 block py-2.5 px-4 rounded`}
+          className={({ isActive }) => `
+    ${isActive ? "bg-white" : ""} 
+    flex items-center space-x-4 block py-2.5 px-4 rounded
+  `}
           end
         >
-          <FaTachometerAlt />
-          <span>Dashboard</span>
+          {({ isActive }) => (
+            <>
+              <FaTachometerAlt className={`${isActive ? 'text-custom-purple' : 'text-white'}`} />
+              <span className={`${isActive ? 'text-custom-purple' : 'text-white'}`}>Dashboard</span>
+            </>
+          )}
         </NavLink>
 
-        <NavLink 
+        <NavLink
           to={`/employee-dashboard/profile/${user._id}`}
-          className={({ isActive }) => `${isActive ? "bg-teal-500" : ""} flex items-center space-x-4 block py-2.5 px-4 rounded`}
+          className={({ isActive }) => `
+    ${isActive ? "bg-white" : ""} 
+    flex items-center space-x-4 block py-2.5 px-4 rounded
+  `}
         >
-          <FaUsers />
-          <span>My Profile</span>
+          {({ isActive }) => (
+            <>
+              <FaUsers className={`${isActive ? 'text-custom-purple' : 'text-white'}`} />
+              <span className={`${isActive ? 'text-custom-purple' : 'text-white'}`}>My Profile</span>
+            </>
+          )}
         </NavLink>
 
-        <NavLink 
+        <NavLink
           to={`/employee-dashboard/leaves/${user._id}`}
-          className={({ isActive }) => `${isActive ? "bg-teal-500" : ""} flex items-center space-x-4 block py-2.5 px-4 rounded`}
+          className={({ isActive }) => `
+    ${isActive ? "bg-white" : ""} 
+    flex items-center space-x-4 block py-2.5 px-4 rounded
+  `}
         >
-          <FaBuilding />
-          <span>CheckSheet Fill</span>
+          {({ isActive }) => (
+            <>
+              <FaBuilding className={`${isActive ? 'text-custom-purple' : 'text-white'}`} />
+              <span className={`${isActive ? 'text-custom-purple' : 'text-white'}`}>CheckSheet Fill</span>
+            </>
+          )}
         </NavLink>
 
-        <NavLink 
+        <NavLink
           to="/employee-dashboard/setting"
-          className={({ isActive }) => `${isActive ? "bg-teal-500" : ""} flex items-center space-x-4 block py-2.5 px-4 rounded`}
+          className={({ isActive }) => `
+    ${isActive ? "bg-white" : ""} 
+    flex items-center space-x-4 block py-2.5 px-4 rounded
+  `}
         >
-          <FaCogs />
-          <span>Settings</span>
+          {({ isActive }) => (
+            <>
+              <FaCogs className={`${isActive ? 'text-custom-purple' : 'text-white'}`} />
+              <span className={`${isActive ? 'text-custom-purple' : 'text-white'}`}>Settings</span>
+            </>
+          )}
         </NavLink>
       </div>
     </div>
