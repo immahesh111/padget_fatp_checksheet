613fb20b9bc363533e1fb0f5876090973a532f50
immahesh555 <themahiece@gmail.com>
2024-12-12 12:15:29 +0530

    ;la

--- src/components/dashboard/AdminSidebar.jsx
+++ src/components/dashboard/AdminSidebar.jsx
@@ -72,7 +72,7 @@ const AdminSidebar = ({ isOpen }) => {
         </NavLink>
 
         <NavLink
-          to="/admin-dashboard/leaves"
+          to="/admin-dashboard/leaves1"
           className={({ isActive }) => `
     ${isActive ? "bg-white" : ""} 
     flex items-center space-x-4 block py-2.5 px-4 rounded
