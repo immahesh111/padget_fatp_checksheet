9cebbcb77a4084b5e8d6847ec3c7221fced1ddf5
immahesh555 <themahiece@gmail.com>
2025-01-06 14:41:21 +0530

    auth

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,62 +1,62 @@
-import React, {createContext,useContext, useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const userContext = createContext()
-const authContext = ({children}) => {
-    const [user, setUser] =useState(null)
-    const [loading, setLoading] = useState(true)
-    
+const userContext = createContext();
 
-    useEffect(() => {
-        const verifyUser = async () => {
-           
-            try{
-                const token = localStorage.getItem('token')
-                if(token){
-                 
-                const response = await axios.get('https://fatp-api.onrender.com/api/auth/verify', {
-                    headers: {
-                        Authorization : `Bearer ${token}`,
-                    },
-                }) ;
-                console.log(response)
-                if(response.data.success) {
-                    setUser(response.data.user)
-                }
-            } else{
-               setUser(null)
-               setLoading(false)
-            }
+const authContext = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (token) {
+          const response = await axios.get('https://fatp-api.onrender.com/api/auth/verify', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          console.log(response);
+          if (response.data.success) {
+            setUser(response.data.user);
+          }
+        } else {
+          setUser(null);
+          setLoading(false);
         }
-             catch(error){
-                console.log(error)
-                if(error.response && !error.response.data.error){
-                    setUser(null)
-                }
-             } finally {
-                setLoading(false)
-             }
+      } catch (error) {
+        console.log(error);
+        if (error.response && !error.response.data.error) {
+          setUser(null);
         }
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        verifyUser()
-    },[]);
+    verifyUser();
+  }, []);
 
-    const login = (user) => {
-        setUser(user)
-    }
+  const login = (user) => {
+    setUser(user);
+  };
 
-    const logout = () => {
-        setUser(null)
-        localStorage.removeItem("token")
-    }
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
 
   return (
-   <userContext.Provider value={{user, login, logout, loading}}>
-        {children}
-   </userContext.Provider>
-  )
-}
+    <userContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </userContext.Provider>
+  );
+};
 
-export const useAuth =() => useContext(userContext)
+export const useAuth = () => useContext(userContext);
 
-export default authContext
+export default authContext;
