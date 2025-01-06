1824fcf66470b5d5cd2aad30d9b6909019beb656
immahesh555 <themahiece@gmail.com>
2025-01-06 14:51:53 +0530

    log

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,11 +1,12 @@
 import React, {createContext,useContext, useEffect, useState } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 const userContext = createContext()
 const authContext = ({children}) => {
     const [user, setUser] =useState(null)
     const [loading, setLoading] = useState(true)
-    
+    const navigate = useNavigate();
 
     useEffect(() => {
         const verifyUser = async () => {
