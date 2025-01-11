97d3c31c0d2881d97212fcf5fbed6af386b84975
immahesh555 <themahiece@gmail.com>
2025-01-11 15:11:57 +0530

    appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import LeaveList8 from './components/leave/List8'
 import LeaveList9 from './components/leave/List9'
 import LeaveList10 from './components/leave/List10'
 import LeaveList11 from './components/leave/List11'
+import LeaveList12 from './components/leave/List12'
 
 import AddLeave from './components/leave/Add'
 import AddLeave2 from './components/leave/Add2'
@@ -38,6 +39,8 @@ import AddLeave8 from './components/leave/Add8'
 import AddLeave9 from './components/leave/Add9'
 import AddLeave10 from './components/leave/Add10'
 import AddLeave11 from './components/leave/Add11'
+import AddLeave12 from './components/leave/Add12'
+
 import Setting from './components/EmployeeDashboard/Setting';
 
 import Table from './components/leave/Table';
@@ -51,6 +54,7 @@ import Table7 from './components/leave/Table7'
 import Table8 from './components/leave/Table8'
 import Table9 from './components/leave/Table9'
 import Table10 from './components/leave/Table10'
+import Table11 from './components/leave/Table11'
 
 import Detail from './components/leave/Detail';
 import Detail1 from './components/leave/Detail1';
@@ -63,6 +67,7 @@ import Detail7 from './components/leave/Detail7';
 import Detail8 from './components/leave/Detail8';
 import Detail9 from './components/leave/Detail9';
 import Detail10 from './components/leave/Detail10';
+import Detail11 from './components/leave/Detail11';
 
 
 function App() {
@@ -103,6 +108,7 @@ function App() {
           <Route path="/admin-dashboard/leaves8" element={<Table8 />}></Route>
           <Route path="/admin-dashboard/leaves9" element={<Table9 />}></Route>
           <Route path="/admin-dashboard/leaves10" element={<Table10 />}></Route>
+          <Route path="/admin-dashboard/leaves11" element={<Table11 />}></Route>
 
           <Route path="/admin-dashboard/leaves/:id" element={<Detail />}></Route>
           <Route path="/admin-dashboard/leaves1/:id" element={<Detail1 />}></Route>
@@ -115,6 +121,7 @@ function App() {
           <Route path="/admin-dashboard/leaves8/:id" element={<Detail8 />}></Route>
           <Route path="/admin-dashboard/leaves9/:id" element={<Detail9 />}></Route>
           <Route path="/admin-dashboard/leaves10/:id" element={<Detail10 />}></Route>
+          <Route path="/admin-dashboard/leaves11/:id" element={<Detail11 />}></Route>
 
 
           <Route path="/admin-dashboard/employees/leaves/:id" element={<LeaveList />}></Route>
@@ -128,6 +135,7 @@ function App() {
           <Route path="/admin-dashboard/employees/leaves8/:id" element={<LeaveList9 />}></Route>
           <Route path="/admin-dashboard/employees/leaves9/:id" element={<LeaveList10 />}></Route>
           <Route path="/admin-dashboard/employees/leaves10/:id" element={<LeaveList11 />}></Route>
+          <Route path="/admin-dashboard/employees/leaves11/:id" element={<LeaveList12 />}></Route>
 
           <Route path="/admin-dashboard/setting" element={<Setting />}></Route>
         </Route>
@@ -154,6 +162,7 @@ function App() {
           <Route path='/employee-dashboard/leaves8/:id' element={<LeaveList9/>}></Route>
           <Route path='/employee-dashboard/leaves9/:id' element={<LeaveList10/>}></Route>
           <Route path='/employee-dashboard/leaves10/:id' element={<LeaveList11/>}></Route>
+          <Route path='/employee-dashboard/leaves11/:id' element={<LeaveList12/>}></Route>
 
           <Route path='/employee-dashboard/add-leave' element={<AddLeave />}></Route>
           <Route path='/employee-dashboard/add-leave2' element={<AddLeave2 />}></Route>
@@ -166,6 +175,7 @@ function App() {
           <Route path='/employee-dashboard/add-leave9' element={<AddLeave9 />}></Route>
           <Route path='/employee-dashboard/add-leave10' element={<AddLeave10 />}></Route>
           <Route path='/employee-dashboard/add-leave11' element={<AddLeave11 />}></Route>
+          <Route path='/employee-dashboard/add-leave12' element={<AddLeave12 />}></Route>
 
           <Route path='/employee-dashboard/setting' element={<Setting />}></Route>
 
